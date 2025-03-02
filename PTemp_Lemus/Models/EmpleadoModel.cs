2e91a1cb3d4db40241a367b636252f96e8a35dc9
Natsuho-1 <santosronaldolemustorres@gmail.com>
2025-03-01 22:17:54 -0600

    first commit

---
+++ PTemp_Lemus/Models/EmpleadoModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PTemp_Lemus.Models
+{
+    [Table("c_Empleado")]
+    public class EmpleadoModel
+    {
+        [Key]
+        public int idEmpleado {  get; set; }
+
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+        public string usuario { get; set; }
+        public string clave { get; set; }
+        public bool activo { get; set; }
+    }
+}
---
+++ PTemp_Lemus/Models/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace PTemp_Lemus.Models
+{
+    public class MyDbContext:DbContext
+    {
+        public MyDbContext() : base("name=DBTemp_Lemus")
+        {
+
+        }
+        public DbSet <EmpleadoModel> EmpleadoModel { get; set; }
+    }
+}
